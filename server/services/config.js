426262086b070c2c34dc618c54692d95aaf7c3bb
Dario Lostumbo <dario.lostumbo@hotmail.com>
2020-12-15 11:18:19 +0100

    finished

--- server/services/config.js
+++ server/services/config.js
@@ -4,6 +4,6 @@ if (process.env.NODE_ENV==='production '){
     console.log('process if',process.env)
     module.exports=require('./prod')
 }else{
-    console.log('process else',process.env.NODE_ENV)
+    console.log('process else',process.env)
     module.exports=require('./dev')
 }
