515f6bc2502686a05f8fc4a48c36b338692538b8
Dario Lostumbo <dario.lostumbo@hotmail.com>
2020-12-15 21:26:07 +0100

    heroku8

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('client/build'))
     const path=require('path')
 
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'))
     })
 }else{
     console.log('IN ELSE',process.env)
